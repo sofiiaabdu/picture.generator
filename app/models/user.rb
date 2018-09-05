24ea743acf9fda2294ead629e7eb3a8f9d1e3a06
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-06 01:14:26 +0300

    Added a feature to dispay the validation error.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_many :pictures
 
   enum sex: { male: 0, female: 1, other: 2 }
+
+  validates :first_name, length: { in: 1..100,
+                                   too_long: "%{count} characters is the maximum allowed"}
+  validates :last_name, length: { in: 1..100,
+                                  too_long: "%{count} characters is the maximum allowed"}
+  validates :about, length: { maximum: 300,
+                              too_long: "%{count} characters is the maximum allowed"}
+  validates :age, numericality: { only_integer: true, greater_than: 0, less_than: 120,
+                                  message: "%{value} seems wrong, it must be less than 120" }
+
 end
