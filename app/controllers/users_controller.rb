23e6d67894bd6cbca0d1d8e4d9742d88f491bd85
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-10 15:47:29 +0300

    Add access admin/user functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ require 'json'
 
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_admin, only: :index
+  before_action :user_access
 
   # GET /users
   # GET /users.json
@@ -95,12 +97,6 @@ class UsersController < ApplicationController
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-=begin
-      if params[:id] = 'sign_out'
-        sign_out current_user
-        redirect_to '/pictures'
-      else
-=end
         @user = User.find(params[:id])
     end
 
@@ -109,4 +105,19 @@ class UsersController < ApplicationController
       params.require(:user).permit(:first_name, :last_name, :sex, :age, :about,
                                    address_attributes: [:zip, :city, :street, :house_member])
     end
-end
+
+  def authorize_admin
+    return unless !current_user.admin?
+    redirect_to root_path, alert: 'Access denied!'
+  end
+
+  def user_access
+    # Check the params hash to see if the passed :id matches the current user's id
+    # (note the .to_i on params[:id], as you are comparing against a Fixnum)
+    # unless params[:id].to_i == current_user.id
+    unless @user == current_user
+      # This line redirects the user to the previous action
+      redirect_to pictures_path
+    end
+  end
+  end
