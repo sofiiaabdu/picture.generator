20dace5a76c6200a891a4fb132a995911992bd64
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-14 17:19:05 +0300

    Working progress.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,5 +3,4 @@ class Picture < ApplicationRecord
   has_many :favourites, dependent: :destroy
 
   scope :pictures_by_user, -> (id) { where(user_id: id) }
-  scope :picture_by_user_and_id, -> (user_id, picture_id) { where(user_id: user_id) && where(id: picture_id)}
 end
