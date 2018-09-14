a082004ac531a05e10e0480de5aa31eb49947adf
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-14 17:08:30 +0300

    Fixed remarks about picture #show action, destroy method.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,22 +2,25 @@ require 'json'
 require 'net/http'
 
 class PicturesController < ApplicationController
-  before_action :authenticate_user!, except: :all_pictures
+  before_action :authenticate_user!, except: :index
 
   # GET /pictures
   # GET /pictures.json
   def index
-    @all_pictures = Picture.all
+    @all_pictures = if params[:filter] == 'user' && current_user
+        current_user.pictures
+      else
+        Picture.all
+      end
   end
 
   def show
-    @pictures = current_user.pictures.all
-    @favourites = current_user.favourites.build
   end
 
   def destroy
-    @picture = Picture.find(params[:picture_id])
-    @picture.destroy
+    # @picture = Picture.find(params[:picture_id])
+    picture =  Picture.find_by(id: params[:picture_id], user_id: current_user.id)
+    picture.destroy
     respond_to do |format|
       format.html { redirect_to user_path(current_user.id) }
     end
